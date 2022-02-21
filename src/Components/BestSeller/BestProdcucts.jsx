a57ea6bf6cab2e7f11b355a5705322d2f784491d
Zizi <valeriaziegler21@gmail.com>
2022-02-21 16:46:17 -0500

    Error ortografico

--- src/Components/BestSeller/BestProdcucts.jsx
+++ src/Components/BestSeller/BestProdcucts.jsx
@@ -33,7 +33,7 @@ export const BestSeller = [
       id: 9,
       name: "Collar Afroditas",
       image: "https://drive.google.com/uc?export=download&id=1ExYaV6xrmOXitw15SAuN4RwHPbT2Jg1T",
-      price: "$5",
+      price: 5,
       url : "",
       size: "Unica",
       material: "Acero Inoxidable",
